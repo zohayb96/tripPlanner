a9ae1b35ae97cfff31a0003de06ef6cd81d663c9
Zohayb Shaikh <zohayb1996@gmail.com>
2018-06-19 16:58:23 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoiem9oYXlic2hhaWtoIiwiYSI6ImNqaW01bnI5cDAwbWYzcXA4djRkNjZ5bWwifQ.aSQjHTxc2IwK8hcHGzoIWA';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
 console.log('running');
